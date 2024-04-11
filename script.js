bc96ef069c9dacad8e87637ff41f93d20e1a4173
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 14:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function volume_sphere() {
     //Write your code here
-  
+	const r = document.getElementById("radius");
+	let v = document.getElementById("vaolume"); 
+	let btn = document.getElementById("submit");
+	btn.addEventListener("click",function () {
+		const radius = parseFloat(r.value);
+
+            // Calculate the volume of the sphere
+            const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+            // Round the volume to four decimal places
+            const roundedVolume = volume.toFixed(4);
+
+            // Display the calculated volume
+            volume.textContent = "Volume: " + roundedVolume;
+	})
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
